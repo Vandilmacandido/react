61282264f872264683845c68eb1dc54afcd8874a
Vandilma Candido <90811919+Vandilmacandido@users.noreply.github.com>
2022-12-15 16:34:15 -0300

    Create conversor.js

---
+++ src/components/conversor.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+
+export default class conversor extends Component {
+  render() {
+    return (
+        <div className='conversor'>
+            <p>Esse é o nosso conversor</p>
+
+        </div>
+    )
+  }
+}
